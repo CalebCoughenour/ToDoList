dd74d014b1afacbbb7ad4cf487d8ce4ce74b293d
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-05-04 11:11:44 -0700

    add working UI that creates a todo list, views it and exits program
    co-authored-by: Jake Edgar <jakeedgar1012@gmail.com>

---
+++ ToDoList/Program.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using ToDoList.Models;
+
+public class Program 
+{
+  public static void Main()
+  {
+    while (true)
+  {
+    Console.WriteLine("Would you like to add something to your Add to your List or View it? add/view/exit");
+    string userInput = Console.ReadLine();
+    if (userInput == "exit")
+    {
+      return;
+    }
+    else if (userInput == "add") 
+    {
+      Console.WriteLine("What would you like to add to the list?");
+      string description = Console.ReadLine();
+      Item userItem = new Item(description);
+      string result = userItem.Description;
+      List<Item> newList = new List<Item> { userItem };
+    }
+    else if (userInput == "view")
+    {
+      foreach (Item item in Item.GetAll())
+      {
+        Console.WriteLine(item.Description);
+      }
+    }
+    else 
+    {
+      Console.WriteLine("Please enter a valid entry.");
+    }
+  }
+  }
+}
