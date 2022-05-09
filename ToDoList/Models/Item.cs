65706500d476e4677c6241e9e792fd6c7d734689
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-05-09 11:16:17 -0700

    begin changing file to ASP.NET MVC

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -5,18 +5,12 @@ namespace ToDoList.Models
   public class Item
   {
     public string Description { get; set; }
-    public int Priority {get; set;}
     private static List<Item> _instances = new List<Item> {};
 
-    public Item(string description)
+    public Item (string description)
     {
       Description = description;
       _instances.Add(this);
-    }
-    public Item(string description, int priority)
-      : this(description)
-    {
-      Priority = priority;
     }
 
     public static List<Item> GetAll()
@@ -28,5 +22,6 @@ namespace ToDoList.Models
     {
       _instances.Clear();
     }
+
   }
 }
--- ToDoList/Program.cs
+++ ToDoList/Program.cs
@@ -1,46 +1,20 @@
-using System;
-using System.Collections.Generic;
-using ToDoList.Models;
-using System.Linq;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
 
-public class Program 
+namespace ToDoList
 {
-  public static void Main()
+  public class Program
   {
-    while (true)
-  {
-    Console.WriteLine("Would you like to add something to your Add to your List or View it? add/view/exit");
-    string userInput = Console.ReadLine();
-    if (userInput == "exit")
+    public static void Main(string[] args)
     {
-      return;
+      var host = new WebHostBuilder()
+        .UseKestrel()
+        .UseContentRoot(Directory.GetCurrentDirectory())
+        .UseIISIntegration()
+        .UseStartup<Startup>()
+        .Build();
+
+      host.Run();
     }
-    else if (userInput == "add") 
-    {
-      Console.WriteLine("What would you like to add to the list?");
-      string description = Console.ReadLine();
-      Item userItem = new Item(description);
-      string result = userItem.Description;
-      List<Item> newList = new List<Item> { userItem };
-    }
-    else if (userInput == "view")
-    {
-      if ( !Item.GetAll().Any() )
-      {
-        Console.WriteLine("Nothing to see here."); 
-      }
-      else 
-      {
-        foreach (Item item in Item.GetAll())
-        {
-          Console.WriteLine(item.Description);
-        }
-      } 
-    }
-    else 
-    {
-      Console.WriteLine("Please enter a valid entry.");
-    }
-  }
   }
 }
