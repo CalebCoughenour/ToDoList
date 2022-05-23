e7f4fdbfbdbf1336fa7288ad15280fecd05ebcae
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-05-23 10:39:26 -0700

    add date and completed section for items
    Co-authored-by: evmeshuris <evmeshuris@users.noreply.github.com>
    Co-authored-by: Mark McConnell <mark.programming1@gmail.com>
    Co-authored-by: Meron G.Tekie <mtekiesd210309@gmail.com>

---
+++ ToDoList/Migrations/20220523163123_Second.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ToDoList.Migrations
+{
+    public partial class Second : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Date",
+                table: "Items",
+                type: "longtext CHARACTER SET utf8mb4",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Date",
+                table: "Items");
+        }
+    }
+}
